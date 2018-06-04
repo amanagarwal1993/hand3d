e375500b6a35b2c7f330ed60c4c03d6a3cf122f6
Aman Agarwal <yuying@protonmail.ch>
2018-06-04 17:09:25 +0530

    Modified pipeline to only use 2D inference

--- nets/ColorHandPose3DNetwork.py
+++ nets/ColorHandPose3DNetwork.py
@@ -45,7 +45,7 @@ class ColorHandPose3DNetwork(object):
         import pickle
 
         if weight_files is None:
-            weight_files = ['./weights/handsegnet-rhd.pickle', './weights/posenet3d-rhd-stb-slr-finetuned.pickle']
+            weight_files = ['./weights/handsegnet-rhd.pickle', './weights/posenet-rhd-stb-slr-finetuned.pickle']
 
         # Initialize with weights
         for file_name in weight_files:
